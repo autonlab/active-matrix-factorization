35b2c95efeb543aecd91689d1c7cc75be265dfef
Dougal Sutherland <dougal@gmail.com>
2012-04-23 17:29:51 -0400

    select lowest entropy with 1step lookahead

---
+++ ratingconcentration/select_1step_lowest_entropy.m
@@ -0,0 +1,42 @@
+function [i, j, evals] = select_1step_lowest_entropy(...
+    Xtr, mask, P, ~, vals, featureFunc, lagrange, delta)
+% Do one-step lookahead and select the point that results in the
+% lowest maxent solution entropy.
+%
+% TODO: support not evaluating every possibility for every query point
+
+[M, N] = size(mask);
+[I, J] = find(mask);
+
+expectations = nan(1, length(I));
+
+for idx = 1 : length(I)
+    exp = 0;
+    
+    this_i = I(idx);
+    this_j = J(idx);
+    
+    new_mask = mask;
+    new_mask(this_i, this_j) = 0;
+    
+    new_Xtr = Xtr;
+    
+    for val_i = 1 : length(vals)        
+        new_Xtr(this_i, this_j) = vals(val_i);
+        [~, new_P] = ratingconcentration(...
+            new_Xtr, new_mask, featureFunc, delta, lagrange);
+        
+        query_P = new_P(new_mask);
+        entropy = -sum(query_P .* log(query_P));
+        
+        exp = exp + P(idx, val_i) * entropy;
+    end
+    
+    expectations(idx) = exp;
+end
+
+evals = sparse(I, J, expectations, M, N);
+[~, idx] = min(expectations(:));
+i = I(idx);
+j = J(idx);
+end
