8b10634f9befb4c293e9527cf993f04b9bcdbca8
Dougal Sutherland <dougal@gmail.com>
2012-05-03 21:29:50 -0400

    test exp_dotprod_sq also

--- python-pmf/test_normal_exps.py
+++ python-pmf/test_normal_exps.py
@@ -2,7 +2,7 @@ import numpy as np
 from functools import partial
 
 import normal_exps as pure
-import normal_exps_cy as cy
+import normal_exps_cy as cyth
 from active_pmf import project_psd
 
 NUM_SAMPS = 5 * 10**5
@@ -26,7 +26,7 @@ def check_expectation(dim, exp_fn, monte_fn=np.prod,
 
         monte = _mean(monte_fn(row) for row in samps)
         exp1 = getattr(pure, exp_fn)(mn, cov, *range(dim))
-        exp2 = getattr(cy, exp_fn)(mn, cov, *range(dim))
+        exp2 = getattr(cyth, exp_fn)(mn, cov, *range(dim))
 
         assert exp1 - exp2 < 1e-7
         assert abs(monte - exp1) / exp1 < .02, "%r - %r" % (monte, exp1)
@@ -42,3 +42,22 @@ def test_exp_squared():
 
 def test_exp_a2bc():
     check_expectation(3, 'exp_a2bc', lambda row: row[0]**2 * row[1] * row[2])
+
+def test_exp_dotprod_sq():
+    n = 1
+    m = 1
+    d = 3
+    k = (n + m) * d
+    u = np.arange(0, n * d).reshape(n, d).T
+    v = np.arange(n * d, (n+m) * d).reshape(m, d).T
+    for i in range(NUM_TIMES):
+        mn = np.random.normal(0, 10, (k,))
+        cov = project_psd(np.random.normal(0, 5, (k,k)), 1e-5)
+        samps = np.random.multivariate_normal(mn, cov, NUM_SAMPS)
+
+        monte = _mean(np.dot(x[u[:,0]], x[v[:,0]])**2 for x in samps)
+        exp1 = pure.exp_dotprod_sq(u, v, mn, cov, 0, 0)
+        exp2 = cyth.exp_dotprod_sq(u, v, mn, cov, 0, 0)
+
+        assert exp1 - exp2 < 1e-7
+        assert abs(monte - exp1) / exp1 < .02, "%r - %r" % (monte, exp1)
