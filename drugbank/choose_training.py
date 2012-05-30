420c10a4ab569c47af5a94ff2dd6425af9d7da31
Dougal Sutherland <dougal@gmail.com>
2012-05-30 00:49:06 -0400

    drugbank chooser improvements

--- drugbank/choose_training.py
+++ drugbank/choose_training.py
@@ -11,39 +11,30 @@ sys.path.append('../python-pmf')
 import get_criteria
 
 outfile = sys.argv[1]
-infile = sys.argv[3] if len(sys.argv) > 3 else 'top_hundred.npy'
+num_pos = int(sys.argv[2])
+num_neg = int(sys.argv[3])
+assert num_neg >= 1
+infile = sys.argv[4] if len(sys.argv) > 4 else 'top_hundred.npy'
 
 with (gzip.open if infile.endswith('.gz') else open)(infile) as f:
-    real = np.load(f)
+    real = np.load(f).astype(int) * 2 - 1 # turn zeros into negatives
 
-if len(sys.argv) > 2:
-    num_to_pick = int(sys.argv[2])
-else:
-    num_to_pick = int(np.round(real.size * .05))
+assert set(real.flat) == {-1, 1}
 
-# assume all non-entries are negatives
-assert set(real.flat) == {0, 1}
-real[real == 0] = -1
-
-knowable = real != 0
 known = np.zeros(real.shape, bool)
 
-# choose at least one rating from every row and column
-for j in np.logical_not(known.sum(axis=0)).nonzero()[0]:
-    i = random.choice(list(knowable[:,j].nonzero()[0]))
-    known[i, j] = 1
-    knowable[i, j] = 0
+# choose the passed number of positives
+positives = (real == 1).reshape(-1)
+pos_indices = list(positives.nonzero()[0])
+pos_picked = random.sample(pos_indices, num_pos)
+known.flat[pos_picked] = 1
 
-for i in np.logical_not(known.sum(axis=1)).nonzero()[0]:
-    j = random.choice(list(knowable[i,:].nonzero()[0]))
-    known[i, j] = 1
-    knowable[i, j] = 0
+# choose the passed number of negatives
+known[-1,-1] = 1
+neg_indices = list(np.logical_not(positives).nonzero()[0])
+neg_picked = random.sample(neg_indices, num_neg-1)
+known.flat[neg_picked] = 1
 
-# choose the rest to round it out to x% of the full dataset
-knowable_indices = list(knowable.reshape(-1).nonzero()[0])
-num_to_pick -= known.sum()
-picked = random.sample(knowable_indices, num_to_pick)
-known.flat[picked] = 1
 
 # make the ratings matrix, rating_vals
 ratings = get_criteria.make_ratings(real, known)
