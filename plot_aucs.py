308118197e6d6936bea9c82ff604ea55d45d4012
Dougal Sutherland <dougal@gmail.com>
2013-02-06 12:07:30 -0500

    clean up plot_aucs code a bunch

--- plot_aucs.py
+++ plot_aucs.py
@@ -13,58 +13,129 @@ import numpy as np
 from plot_results import (KEY_NAMES, ActivePMF, BayesianPMF, BPMF,  # for pickle
                           linestyle_color_marker, guess_kind)
 
+################################################################################
+### loading data
+
 warn = functools.partial(print, "WARNING:", file=sys.stderr)
 
 _warned_about = set()
-def load_results(filenames):
-    for filename in filenames:
-        with open(filename, 'rb') as f:
-            r = pickle.load(f)
-        kind = guess_kind(filename)
+def load_results(filename):
+    with open(filename, 'rb') as f:
+        r = pickle.load(f)
+    kind = guess_kind(filename)
 
-        if all(k.startswith('_') for k in r):
-            if filename not in _warned_about:
-                warn("No data in {}".format(filename))
-            _warned_about.add(filename)
+    if all(k.startswith('_') for k in r):
+        if filename not in _warned_about:
+            warn("No data in {}".format(filename))
+        _warned_about.add(filename)
 
-        if kind == 'apmf':
-            yield r
-        else:
-            rep = re.compile(r'^(?!(_|{}_))'.format(kind))
-            yield {rep.sub(kind + '_', k): v for k, v in r.items()}
+    if kind == 'apmf':
+        return r
+    else:
+        rep = re.compile(r'^(?!(_|{}_))'.format(kind))
+        return {rep.sub(kind + '_', k): v for k, v in r.items()}
+
+
+def load_data(filenames, do_rmse=False, do_rmse_auc=False,
+                         do_cutoffs=None, do_cutoff_aucs=None):
+    desired_ns = None
+
+    want_rmses = do_rmse or do_rmse_auc
+    if want_rmses:
+        rmse_traces = defaultdict(list)
+
+    cutoff_vals = set()
+    if do_cutoffs:
+        cutoff_vals.update(do_cutoffs)
+    if do_cutoff_aucs:
+        cutoff_vals.update(do_cutoff_aucs)
+
+    if cutoff_vals:
+        cutoff_traces = defaultdict(functools.partial(defaultdict, list))
+
+    if not cutoff_vals and not results:  # not asked to do anything!
+        return {}
+
+    for r in map(load_results, filenames):
+        if cutoff_vals:
+            real = r['_real']
+            ratings = r['_ratings']
+
+        for k, v in r.items():
+            if k.startswith('_'):
+                continue
+
+            ns, rmses, ijs, evals = zip(*v)
+            ns = np.asarray(ns)
+            if desired_ns is not None:
+                assert np.all(ns == desired_ns)
+            else:
+                desired_ns = ns
+
+            if want_rmses:
+                rmse_traces[k].append(rmses)
+
+            if cutoff_vals:
+                assert ijs[0] is None
+                for cutoff in cutoff_vals:
+                    poses = [(ratings[:, 2] >= cutoff).sum()]
+                    for i, j in ijs[1:]:
+                        poses.append(1 if real[i, j] >= cutoff else 0)
+                    cutoff_traces[cutoff][k].append(np.cumsum(poses))
+
+    results = {'ns': desired_ns}
+
+    if do_rmse:  # name => mean RMSE curve
+        results['rmse'] = {
+            k: np.mean(v, axis=0) for k, v in rmse_traces.items()
+        }
+
+    if do_rmse_auc:  # name => array of area under RMSE curves
+        results['rmse_auc'] = {
+            k: np.trapz(v, axis=1) for k, v in rmse_traces.items()
+        }
+
+    if do_cutoffs:  # cutoff => name => curve of # positives
+        results['cutoffs'] = {
+            cutoff: {k: np.mean(v, axis=0) for k, v in c_vals.items()}
+            for cutoff, c_vals in cutoff_traces.items()
+        }
+
+    if do_cutoff_aucs:  # cutoff => name => array of AUC for curves of # pos
+        results['cutoff_aucs'] = {
+            cutoff: {k: np.trapz(v, axis=1) for k, v in c_vals.items()}
+            for cutoff, c_vals in cutoff_traces.items()
+        }
+
+    return results
 
 
 ################################################################################
+### plotting
+
+def show_legend(where='outside', fontsize=11):
+    import matplotlib.pyplot as plt
+    from matplotlib.font_manager import FontProperties
+
+    if where == 'outside':
+        ax = plt.gca()
+        box = ax.get_position()
+        ax.set_position([box.x0, box.y0, box.width * .7, box.height])
+        plt.legend(loc='center left', bbox_to_anchor=(1, .5),
+                   prop=FontProperties(size=10))
+    else:
+        plt.legend(loc='best', prop=FontProperties(size=fontsize))
 
 
-def rmse(result):
-    ns, rmses, ijs, evals = zip(*result)
-    return np.asarray(ns), np.asarray(rmses)
-
-
-def plot_rmses(filenames):
+def plot_lines(ns, data, ylabel=None):
     import matplotlib.pyplot as plt
 
     plt.xlabel("# of rated elements")
-    plt.ylabel("RMSE")
-
-    # read in the actual results
-    results = defaultdict(list)
-    prev_ns = None
-    for f, r in zip(filenames, load_results(filenames)):
-        for k, v in r.items():
-            if not k.startswith('_'):
-                ns, rmses = rmse(v)
-                if prev_ns is None:
-                    prev_ns = ns
-                else:
-                    assert np.all(prev_ns == ns)
-
-                results[k].append(rmses)
+    plt.ylabel(ylabel)
 
     nice_results = sorted(
-            ((KEY_NAMES[k], v) for k, v in results.items()),
-            key=lambda kv: np.mean(kv[1], axis=0)[-1], reverse=True)
+            ((KEY_NAMES[k], v) for k, v in data.items()),
+            key=lambda kv: kv[1][-1], reverse=True)
 
     # offset lines a bit so you can see when some of them overlap
     total = len(ns)
@@ -77,166 +148,48 @@ def plot_rmses(filenames):
         nums = ns + (idx - total / 2) * offset
 
         l, c, m = next(l_c_m)
-        plt.plot(nums, np.mean(vals, axis=0),
-                 linestyle=l, color=c, label=nice_name, marker=m)
+        plt.plot(nums, vals, linestyle=l, color=c, label=nice_name, marker=m)
 
     # only show integer values for x ticks
     #xmin, xmax = plt.xlim()
     #plt.xticks(range(math.ceil(xmin), math.floor(xmax) + 1))
 
 
-def show_legend(where='outside', fontsize=11):
-    from matplotlib.font_manager import FontProperties
-    if where == 'outside':
-        ax = plt.gca()
-        box = ax.get_position()
-        ax.set_position([box.x0, box.y0, box.width * .7, box.height])
-        plt.legend(loc='center left', bbox_to_anchor=(1, .5),
-                   prop=FontProperties(size=10))
-    else:
-        plt.legend(loc='best', prop=FontProperties(size=fontsize))
-
-
-def rmse_auc(result):
-    ns, rmses, ijs, evals = zip(*result)
-    ns = np.asarray(ns)
-    rmses = np.asarray(rmses)
-
-    return ((rmses[:-1] + rmses[1:]) * np.diff(ns)).sum() / 2
-
-
-def get_aucs(filenames):
-    results = defaultdict(list)
-    for f, r in zip(filenames, load_results(filenames)):
-        for k, v in r.items():
-            if not k.startswith('_'):
-                results[k].append(rmse_auc(v))
-    return {k: np.array(v) for k, v in results.items()}
-
-
-def plot_aucs(filenames):
+def plot_aucs(aucs, ylabel=None):
     import matplotlib.pyplot as plt
-
-    names, aucs = list(zip(*sorted(
-        (KEY_NAMES[k], v) for k, v in get_aucs(filenames).items())))
+    names, aucs = list(zip(*sorted((KEY_NAMES[k], v) for k, v in aucs.items())))
 
     if all(a.size == 1 for a in aucs):
         plt.plot(aucs, linestyle='None', marker='o')
         indices = np.arange(len(names))
     else:
-        plt.boxplot(aucs)
+        try:
+            from statsmodels.graphics.boxplots import beanplot
+        except ImportError:
+            plt.boxplot(aucs)
+        else:
+            beanplot(aucs, ax=plt.gca(), plot_opts={'cutoff': True})
         indices = np.arange(len(names)) + 1
     plt.xticks(indices, names, rotation=90)
     plt.xlim(indices[0] - .5, indices[-1] + .5)
-    plt.ylabel('AUC (RMSE)')
-    plt.tight_layout()
-
-
-def plot_cutoff_aucs(filenames, cutoff):
-    import matplotlib.pyplot as plt
-
-    names, aucs = zip(*sorted(
-        (KEY_NAMES[k], v) for k, v
-         in get_num_ge_cutoff_auc(filenames, cutoff).items()))
-
-    if all(a.size == 1 for a in aucs):
-        plt.plot(aucs, linestyle='None', marker='o')
-        indices = np.arange(len(names))
-    else:
-        plt.boxplot(aucs)
-        indices = np.arange(len(names)) + 1
-    plt.xticks(indices, names, rotation=90)
-    plt.xlim(indices[0] - .5, indices[-1] + .5)
-    plt.ylabel('AUC (# >= {})'.format(cutoff))
+    plt.ylabel(ylabel)
     plt.tight_layout()
 
 ################################################################################
 
 
-def get_num_ge_cutoff(filenames, cutoff):
-    results = defaultdict(list)
-
-    des_ns = None
-
-    for f, r in zip(filenames, load_results(filenames)):
-        real = r['_real']
-
-        for k, v in r.items():
-            if k.startswith('_'):
-                continue
-
-            ns, rmses, ijs, evals = zip(*v)
-            if des_ns is None:
-                des_ns = ns
-            else:
-                assert np.all(ns == des_ns)
-
-            assert ijs[0] is None
-            poses = [(r['_ratings'][:, 2] >= cutoff).sum()]
-
-            for i, j in ijs[1:]:
-                poses.append(poses[-1] + (1 if real[i, j] >= cutoff else 0))
-
-            results[k].append(np.asarray(poses))
-
-    return results, np.asarray(ns)
-
-
-def get_num_ge_cutoff_mean(filenames, cutoff):
-    results, ns = get_num_ge_cutoff(filenames, cutoff)
-    return {k: np.mean(v, 0) for k, v in results.items()}, ns
-
-
-def get_num_ge_cutoff_auc(filenames, cutoff):
-    results, ns = get_num_ge_cutoff(filenames, cutoff)
-    return {k: np.array([((poses[:-1] + poses[1:]) * np.diff(ns)).sum() / 2
-                for poses in v])
-            for k, v in results.items()}
-
-
-def plot_num_ge_cutoff(filenames, cutoff):
-    import matplotlib.pyplot as plt
-
-    plt.xlabel("# of rated elements")
-    plt.ylabel("# >= {}".format(cutoff))
-
-    # read in the actual results
-    vals, ns = get_num_ge_cutoff(filenames, cutoff)
-    ns = np.asarray(ns)
-    nice_results = sorted(
-            ((KEY_NAMES[k], v) for k, v in vals.items()),
-            key=lambda kv: np.mean(kv[1], axis=0)[-1], reverse=True)
-
-    # offset lines a bit so you can see when some of them overlap
-    total = len(vals)
-    offset = .15 / total
-
-    l_c_m = linestyle_color_marker(len(ns))
-
-    for idx, (nice_name, vals) in enumerate(nice_results):
-        nums = ns + (idx - total / 2) * offset
-
-        l, c, m = next(l_c_m)
-        plt.plot(nums, np.mean(vals, axis=0),
-                 linestyle=l, color=c, label=nice_name, marker=m)
-
-    # only show integer values for x ticks
-    #xmin, xmax = plt.xlim()
-    #plt.xticks(range(math.ceil(xmin), math.floor(xmax) + 1))
-
-
-################################################################################
-
-if __name__ == '__main__':
+def main():
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('files', nargs='+')
 
-    parser.add_argument('--rmses', action='store_true', default=False)
-    parser.add_argument('--no-rmses', action='store_false', dest='rmses')
+    g = parser.add_mutually_exclusive_group()
+    g.add_argument('--rmses', action='store_true', default=False)
+    g.add_argument('--no-rmses', action='store_false', dest='rmses')
 
-    parser.add_argument('--auc', action='store_true', default=True)
-    parser.add_argument('--no-auc', action='store_false', dest='auc')
+    g = parser.add_mutually_exclusive_group()
+    g.add_argument('--auc', action='store_true', default=True)
+    g.add_argument('--no-auc', action='store_false', dest='auc')
 
     parser.add_argument('--ge-cutoff', nargs='+', type=float)
     parser.add_argument('--ge-cutoff-auc', nargs='+', type=float)
@@ -253,24 +206,33 @@ if __name__ == '__main__':
 
     import matplotlib.pyplot as plt
 
+    data = load_data(args.files,
+        do_rmse=args.rmses, do_rmse_auc=args.auc,
+        do_cutoffs=args.ge_cutoff, do_cutoff_aucs=args.ge_cutoff_auc)
+    ns = data['ns']
+
     if args.rmses:
         plt.figure()
-        plot_rmses(args.files)
+        plot_lines(ns, data['rmse'], 'RMSE')
         show_legend(args.legend)
 
     if args.auc:
         plt.figure()
-        plot_aucs(args.files)
+        plot_aucs(data['rmse_auc'], 'AUC (RMSE)')
 
     if args.ge_cutoff:
         for cutoff in args.ge_cutoff:
             plt.figure()
-            plot_num_ge_cutoff(args.files, cutoff)
+            plot_lines(ns, data['cutoffs'][cutoff], '# >= {}'.format(cutoff))
             show_legend(args.legend)
 
     if args.ge_cutoff_auc:
         for cutoff in args.ge_cutoff_auc:
             plt.figure()
-            plot_cutoff_aucs(args.files, cutoff)
+            plot_aucs(data['cutoff_aucs'][cutoff],
+                      'AUC (# >= {})'.format(cutoff))
 
     plt.show()
+
+if __name__ == '__main__':
+    main()
