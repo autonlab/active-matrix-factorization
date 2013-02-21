fix a negative sign in the KL divergence for variational approxes :(

python-pmf/active_pmf.py
@@ -235,7 +235,7 @@ class ActivePMF(ProbabilisticMatrixFactorization):
 
         # entropy term
         det_sign, log_det = np.linalg.slogdet(cov)
-        div += log_det / 2
+        div -= log_det / 2
 
         return div
 